4b5d210305707705824d5018264dbcd11c196fa3
Jason Bell <jason.jbell@yahoo.com>
2022-01-27 14:11:29 -0800

    new branch new me

---
+++ scripts/enemyAi.gd
@@ -0,0 +1,93 @@
+extends KinematicBody2D
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+export var WANDER_RANGE = 4
+
+var knockback = Vector2.ZERO
+var velocity = Vector2.ZERO
+
+enum {
+	IDLE, 
+	WANDER,
+	CHASE
+}
+
+var state = IDLE
+
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var sprite = $AnimatedSprite
+onready var wanderController = $WanderController
+onready var softCollision = $SoftCollision
+onready var stats = $Stats
+onready var label = $HealthUI/Label
+
+func _ready():
+	state = pick_new_state([IDLE, WANDER])
+
+func _physics_process(delta):
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
+	knockback = move_and_slide(knockback)
+	
+	match state: 
+		IDLE: 
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+			
+			if wanderController.get_time_left() == 0: 
+				state = pick_new_state([IDLE, WANDER])
+				wanderController.timer_start(rand_range(1, 3))
+				
+			sprite.animation = "Idle"
+		WANDER: 
+			seek_player()
+			if wanderController.get_time_left() == 0: 
+				state = pick_new_state([IDLE, WANDER])
+				wanderController.timer_start(rand_range(1, 3))
+			
+			var direction = global_position.direction_to(wanderController.target_position)
+			velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			sprite.flip_h = velocity.x < 0
+			sprite.animation = "Run"
+						
+			if global_position.distance_to(wanderController.target_position) <= WANDER_RANGE:
+				state = pick_new_state([IDLE, WANDER])
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null: 
+				var direction = global_position.direction_to(player.global_position)
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+			sprite.animation = "Run"
+		
+	if softCollision.is_colliding():
+		velocity += softCollision.get_push_vector() * delta * 400
+		
+	velocity = move_and_slide(velocity)
+
+func seek_player(): 
+	if playerDetectionZone.can_see_player(): 
+		state = CHASE
+		
+func pick_new_state(stateList):
+	stateList.shuffle()
+	return stateList.pop_front()
+
+func _on_HurtBox_area_entered(area):
+#	var knockback_vector = area.knockback_vector
+
+# UNCOMMENT and delete other line once player is working
+#	stats.health -= area.damage
+	stats.health -= 1
+	knockback = Vector2.RIGHT * 150
+#
+func _on_Stats_no_health():
+	queue_free()
+
+func _on_Stats_update_health():
+	label.text = "HP: " + str(stats.health)
