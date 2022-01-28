3b6096dcc3d7641190dfaea180c3abf962c50b6d
Jason Bell <jason.jbell@yahoo.com>
2022-01-27 18:01:49 -0800

    fixed knockback direction, gave player health and health UI

---
+++ scripts/Player/playerStats.gd
@@ -0,0 +1,14 @@
+extends Node
+export var attackPower = 10
+export(int) var MAX_HEALTH = 5
+onready var health = MAX_HEALTH setget set_health
+
+signal no_health
+signal update_health
+
+func set_health(value):
+	health = value
+	if health <= 0: 
+		emit_signal("no_health")
+	else:
+		emit_signal("update_health")
