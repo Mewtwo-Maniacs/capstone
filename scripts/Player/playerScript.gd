ce28ca513b15f4c659f6a65690711beb769ff6d8
Jason Bell <jason.jbell@yahoo.com>
2022-02-03 18:01:49 -0800

    Level1 pretty much finished without enemies.

--- scripts/Player/playerScript.gd
+++ scripts/Player/playerScript.gd
@@ -91,9 +91,16 @@ func _on_PlayerStats_update_health():
 func _on_SwordHitbox_area_entered(area):
 	pass # Replace with function body.
 
-#TopTeleport
-func _on_Area2D_area_entered(area):
+#TopTeleport in HomeBase
+func _on_TopTele_area_entered(area):
+	get_tree().change_scene("res://scenes/Worlds/Level1.tscn")
+
+#BottomTeleport in HomeBase
+func _on_BotTele_area_entered(area):
+	get_tree().change_scene("res://scenes/Worlds/HomeBase.tscn")
+
+#MainDoor teleport in HomeBase
+func _on_MainDoor_area_entered(area):
 	get_tree().change_scene("res://scenes/main.tscn")
-#BottomTeleport
-func _on_Area2D2_area_entered(area):
-	get_tree().change_scene("res://scenes/main.tscn")
+
+
