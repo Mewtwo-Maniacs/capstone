3e4f9284aa9f9fe8a41a0f0101d07070587856d9
Gulam <g.u.islam@gmail.com>
2022-02-07 01:12:22 -0500

    Added Pause Menu, TODO: Fix GrabFocus Bug

---
+++ scripts/TitleScreen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$mainMenu/Play.grab_focus()
+	
+func _on_Play_pressed():
+	get_tree().change_scene("res://scenes/main.tscn")
+func _on_Login_pressed():
+	get_tree().change_scene("res://scenes/login.tscn")
+func _on_Options_pressed():
+	pass
+func _on_Quit_pressed():
+	get_tree().quit()
