041d9ee3cfd5de77f5e910d1cb3aedd57f682b39
Jason Bell <jason.jbell@yahoo.com>
2022-02-15 16:49:01 -0800

    added zach's office with zach. Added 3rd tele to homebase and fixed sign to be more obvious

--- scripts/World/ZacSign.gd
+++ scripts/World/ZacSign.gd
@@ -3,14 +3,14 @@ extends Area2D
 var active = false
 
 func _ready():
-	connect("body_entered", self, '_on_BotSign_body_entered')
-	connect("body_exited", self, '_on_BotSign_body_exited')
+	connect("body_entered", self, '_on_TopSign_body_entered')
+	connect("body_exited", self, '_on_TopSign_body_exited')
 	
 func _input(event):
 	if get_node_or_null('DialogNode') == null:
 		if event.is_action_pressed("ui_yeet") and active:
 			get_tree().paused = true
-			var dialog = Dialogic.start('botSign')
+			var dialog = Dialogic.start('zacSign')
 			dialog.pause_mode = Node.PAUSE_MODE_PROCESS
 			dialog.connect('timeline_end', self, 'unpause')
 			add_child(dialog)
@@ -18,12 +18,12 @@ func _input(event):
 func unpause(timeline_name):
 	get_tree().paused = false
 
-func _on_BotSign_body_entered(body):
+func _on_TopSign_body_entered(body):
 	if body.name == 'Player':
-		$botSign.text = "Press 'E' to read"
+		$ZacSignLabel.text = "Press 'E' to read"
 		active = true
 
-func _on_BotSign_body_exited(body):
+func _on_TopSign_body_exited(body):
 	if body.name == 'Player':
-		$botSign	.text = ""
+		$ZacSignLabel.text = ""
 		active = false
